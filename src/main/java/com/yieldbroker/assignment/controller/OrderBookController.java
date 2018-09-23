f2684238784251c8cabbe094d8d3e3778d5f124a
Kievondy Tan <kievondy@gmail.com>
2018-09-23 20:15:21 +0800

    Modified 3.3 cancelOrder to delete method

--- src/main/java/com/yieldbroker/assignment/controller/OrderBookController.java
+++ src/main/java/com/yieldbroker/assignment/controller/OrderBookController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.sql.SQLException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,7 +56,7 @@ public class OrderBookController {
 	 * 
 	 * @param clientOrderId
 	 */
-	@PostMapping("/cancelOrder")
+	@DeleteMapping("/cancelOrder")
 	public void cancelOrder(@RequestParam("clientOrderId") int clientOrderId) {
 		orderBookService.cancelOrder(clientOrderId);
 	}
